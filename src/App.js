238831679eff904d8d4a65ad1ba5bf53410d20a8
JE-Enriquez <emmanuel160890@gmail.com>
2026-05-27 15:08:33 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Songs from "./components/Song";
 import style from "../src/css/style.css"
-import React, { Component } from "react";
+import React from "react";
 
 function App() {
 
